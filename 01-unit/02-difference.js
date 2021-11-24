fd0762117f0c8824aad9b1bb6433cb55f2078fb3
Guille <91394918+GuilleCap@users.noreply.github.com>
2021-11-23 20:03:55 -0500

    add differences in diffferent problems

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,9 +1,10 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
+function difference(num1 = 2 , num2 = 2) {
+   return num1 - num2;
+  }
 
-}
 
 // ==================================================
 // DEFINE AND EXECUTE TEST CASES
@@ -14,23 +15,27 @@ try {
   // --------------------------------------------------
   // It should return the difference of two numbers.
   var result = difference(1, 1);
-  if (result !== 0) throw new Error('Expected difference(1, 1) to be 0. Received: ' + result);
+  if (result !== 2) throw new Error('Expected difference(1,1) to be 2. Received:' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = difference(1, 1, 2, 3, 4);
+  if (result !== 2) throw new Error('Expected difference(1, 1, 2, 3, 4) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1);
+  if (result !== 3) throw new Error('Expected difference(1) to be 3. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = difference();
+  if (result !== 2) throw new Error('Expected difference() to be 2. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
